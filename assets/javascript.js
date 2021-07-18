f4f4236aa1665b788efdf87a1f30b40a278e0a6f
Pamulapati Kirthana <85549854+PamulapatiKirthana@users.noreply.github.com>
2021-07-18 16:58:16 +0530

    Add files via upload

---
+++ assets/javascript.js
@@ -0,0 +1,8 @@
+function myFun(){
+     window.alert("Thank You for ordering!");
+    // window.alert("Good job!", "You clicked the button!", "success");
+    // window.confirm('Are you sure you wanna delete this?'); 
+    // window.prompt("Hello custo!");
+
+    window.location.reload()
+}
